ee0f8dc6ac4f42740e0599e88e1b2f093751f589
panu@vooops <panu@vooops>
2009-07-31 17:33:36 +0300

    Took trip index into use.

--- live/trips.js
+++ live/trips.js
@@ -1,9 +1,22 @@
 
 function addTrips(mapConfig, map) {
-  var tripsFilename = "testi.gpx";
-  var polylineEncoder = new PolylineEncoder();
+  var tripsFilename = "trips/index.xml";
 
   GDownloadUrl(tripsFilename, function(data, responseCode) {
+    var xml = GXml.parse(data);
+    var trips = xml.documentElement.getElementsByTagName("trip");
+
+    for (var i = 0; i < trips.length; i++) {
+      var tripFilename = trips[i].getAttribute("filename");
+      addTrip(mapConfig, map, tripFilename);
+    }
+  });
+}
+
+function addTrip(mapConfig, map, tripFilename) {
+  var polylineEncoder = new PolylineEncoder();
+
+  GDownloadUrl(tripFilename, function(data, responseCode) {
     var xml = GXml.parse(data);
     var trks = xml.documentElement.getElementsByTagName("trk");
 
